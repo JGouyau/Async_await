48571a4c965a32149d539a1627bc3f5cc9d8b0e6
JGouyau <Gouyau.jimmy@gmail.com>
2021-11-17 14:48:26 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Task<int>> listeDeTache = new Dictionary<int, Task<int>> { { 1, Tache() }, { 2, Tache() } };
+            Task.WhenAll(listeDeTache.Values);
+            foreach(var tache in listeDeTache)
+            {
+                Console.WriteLine("la tache " + tache.Key + " a attendu "+ tache.Value.Result);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task<int> Tache()
+        {
+            Random rng = new Random();
+            Int32 waitTime = rng.Next(1000, 5000);
+            await Task.Delay(waitTime);
+            return waitTime;
+        }
+    }
+}
